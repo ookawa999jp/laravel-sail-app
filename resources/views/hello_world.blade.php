e4dd94b9f2ddf632493793d8589bf7a11b2cf093
ookawa999jp <ookawa999jp@gmail.com>
2026-02-01 22:47:28 +0900

    カリキュラム、Hello、各種ゲーム用のビューを追加し、ルートを更新しました。
    - カリキュラムビューを作成し、準備、ページ表示、フォーム、データベース、認証のセクションを追加しました。
    - ユーザーに名前とコース名を表示するためのHelloビューを追加しました。
    - シンプルな挨拶を表示するためのhello_worldビューを導入しました。
    - 新規ユーザーのLaravel学習を支援するため、indexビューを開発しました。
    - ビュー間で一貫したデザインを実現するためのレイアウトを実装しました。
    - モンティ・ホール問題の結果を表示するためのmonty-hallビューを追加しました。
    - ユーザーの運勢を表示するためのomikujiビューを作成しました。
    - 様々な都市の現在時刻を表示するためのworld-timeビューを開発しました。
    - ゲームロジック用の新しいビューとコントローラーを追加するために、Webルートを更新しました。

---
+++ resources/views/hello_world.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang='ja' >
+    <head>
+        <meta charset='UTF-8'>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1 style="color:red">Hello World!</h1>
+    </body>
+</html>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\UtilityController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', fn () => view('hello', [
+    'name' => '田中',
+    'course' => 'Laravel',
+]));
+
+Route::get('/', fn () => view('index'));
+Route::get('/curriculum', fn () => view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
