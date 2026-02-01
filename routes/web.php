53bf14168c2ad38f42e1fb77d8165ffa2458e6d4
ookawa999jp <ookawa999jp@gmail.com>
2026-02-02 01:00:43 +0900

    ハイアンドローゲーム機能を追加し、イベント登録機能を改善しました。イベント名のログ出力を修正し、成功メッセージを表示するようにしました。

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\HiLowController;
 use App\Http\Controllers\RequestSampleController;
 use App\Http\Controllers\UtilityController;
 use Illuminate\Support\Facades\Route;
@@ -38,3 +39,12 @@ Route::get('/login', [RequestSampleController::class, 'loginForm']);
 Route::post('/login', [RequestSampleController::class, 'login'])->name('login');
 
 Route::resource('/events', EventController::class)->only(['create', 'store']);
+
+// ハイローゲーム
+Route::get('/hi-low', [HiLowController::class, 'index']);
+Route::post('/hi-low', [HiLowController::class, 'result'])->name('hi-low');
+
+// ハイローゲーム
+// 変。
+// Route::get('/hi-low', [HiLowController::class, 'index'])->name('hi-low');
+// Route::post('/hi-low', [HiLowController::class, 'result']);
